bca43e000612deb02f88c4a5fdc6bbfb916b9af0
Benjamin Davies <bentendavies@gmail.com>
2019-02-24 07:03:40 +0000

    Change the way I open terminals

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,31 +5,32 @@ import XMonad.Layout.Spacing
 import XMonad.Layout.Grid
 import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
-import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.Run(runInTerm)
 import System.IO
 
 main = do
-  spawnPipe "xmodmap ~/.speedswapper"
-  spawnPipe "feh --bg-fill /usr/share/backgrounds/default"
-  spawnPipe "compton"
-  spawnPipe "xmobar"
-  spawnPipe "google-chrome --no-startup-window"
+  spawn "xmodmap ~/.speedswapper"
+  spawn "feh --bg-fill /usr/share/backgrounds/default"
+  spawn "compton"
+  spawn "xmobar"
+  spawn "google-chrome --no-startup-window"
   xmonad $ docks $ defaultConfig
     { manageHook = manageDocks <+> manageHook defaultConfig
     , layoutHook = avoidStruts . noBorders . smartSpacingWithEdge 10
       $ layoutHook defaultConfig ||| Grid
     , modMask = mod4Mask
+    , terminal = "konsole"
     } `additionalKeysP`
     [ ("M-q", kill)
     , ("M-w", sendMessage NextLayout)
     , ("M-e", spawn "pcmanfm-qt")
     , ("M-r", restart "xmonad" True)
-    , ("M-<Return>", spawn term)
+    , ("M-<Return>", runInTerm "" "zsh")
     , ("M-S-<Return>", windows W.swapMaster)
     , ("M-z", spawn "i3lock -i /usr/share/backgrounds/default")
     , ("M-x", spawn "poweroff")
     , ("M-c", spawn "google-chrome")
-    , ("M-v", spawn (term <+> " -e nvim"))
+    , ("M-v", runInTerm "" "nvim")
     , ("M-b", sendMessage ToggleStruts)
     , ("M-m", spawn "minecraft-launcher")
     , ("M-<Space>", spawn "dmenu_run")
@@ -39,5 +40,3 @@ main = do
     , ("<Print>", spawn "scrot")
     , ("C-<Print>", spawn "sleep 0.2; scrot -s")
     ]
-
-term = "konsole" 
