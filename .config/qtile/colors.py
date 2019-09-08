91bd19ca54a6cc8c39b624b4e815f7d7d304f493
Benjamin Davies <bentendavies@gmail.com>
2019-09-08 21:14:22 +1200

    Tinker some more

---
+++ .config/qtile/colors.py
@@ -0,0 +1,12 @@
+# http://terminal.sexy/ /base16/chalk.dark
+
+text = '#d0d0d0'
+background = '#151515'
+black = '#151515'
+red = '#fb9fb1'
+green = '#acc267'
+yellow = '#ddb26f'
+blue = '#6fc2ef'
+magenta = '#e1a3ee'
+cyan = '#12cfc0'
+white = '#d0d0d0'
--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -4,49 +4,62 @@ from libqtile.config import Key, Screen, Group, Drag, Click
 from libqtile.lazy import lazy
 from libqtile import layout, bar, widget, hook
 
-mod = "mod4"
+import colors
 
-background = "/usr/share/backgrounds/gnome/Wood.jpg"
-term = "st"
+mod = 'mod4'
+
+background = '/usr/share/backgrounds/gnome/Wood.jpg'
+term = 'st'
 
 @hook.subscribe.startup_once
 def autostart_once():
-    call("xrdb ~/.Xresources", shell=True)
-    call("xmodmap ~/.speedswapper", shell=True)
+    call('xrdb ~/.Xresources', shell=True)
+    call('xmodmap ~/.speedswapper', shell=True)
+    Popen('redshift -l -38:176 -t 6500:3500', shell=True)
+    Popen('TMUX=TMUX albert', shell=True)
+    Popen('google-chrome-stable --no-startup-window', shell=True)
 
 @hook.subscribe.startup
 def autostart():
-    call("feh --bg-fill " + background, shell=True)
-    Popen("compton", shell=True)
-    Popen("TMUX=TMUX albert", shell=True)
-    Popen("google-chrome-stable --no-startup-window", shell=True)
-    Popen("redshift -l -38:176 -t 6500:3500", shell=True)
+    call('feh --bg-fill ' + background, shell=True)
+    Popen('compton', shell=True)
+
+def lazyBrowser(url, profile=0):
+    profile_dir = 'Default'
+    if profile > 0:
+        profile_dir = f'Profile {profile}'
+
+    return lazy.spawn(f'google-chrome-stable --profile-directory={profile_dir} --new-window {url}')
 
 keys = [
     # Control and power
-    Key([mod], "q", lazy.window.kill()),
-    Key([mod, "shift"], "q", lazy.shutdown()),
-    Key([mod], "r", lazy.restart()),
-    Key([mod], "space", lazy.spawn("albert show")),
-    Key([mod, "shift"], "space", lazy.spawncmd()),
+    Key([mod], 'q', lazy.window.kill()),
+    Key([mod, 'shift'], 'q', lazy.shutdown()),
+    Key([mod], 'r', lazy.restart()),
+    Key([mod], 'space', lazy.spawn('albert toggle')),
+    Key([mod, 'shift'], 'space', lazy.spawncmd()),
 
     # Navigation
-    Key([mod], "j", lazy.layout.down()),
-    Key([mod], "k", lazy.layout.up()),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
-    Key([mod], "Tab", lazy.next_layout()),
-    Key([mod], "t", lazy.window.toggle_floating()),
-    Key([mod], "F11", lazy.window.toggle_fullscreen()),
+    Key([mod], 'j', lazy.layout.down()),
+    Key([mod], 'k', lazy.layout.up()),
+    Key([mod, 'shift'], 'j', lazy.layout.shuffle_down()),
+    Key([mod, 'shift'], 'k', lazy.layout.shuffle_up()),
+    Key([mod], 'Tab', lazy.next_layout()),
+    Key([mod], 't', lazy.window.toggle_floating()),
+    Key([mod], 'F11', lazy.window.toggle_fullscreen()),
 
     # Media
-    Key([], "XF86AudioPlay", lazy.spawn("mpc toggle")),
-    Key([], "XF86AudioMute", lazy.spawn("pactl set-sink-mute 0 toggle")),
-    Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume 0 -2%")),
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume 0 +2%")),
+    Key([], 'XF86AudioPlay', lazy.spawn('mpc toggle')),
+    Key([], 'XF86AudioMute', lazy.spawn('pactl set-sink-mute 0 toggle')),
+    Key([], 'XF86AudioLowerVolume', lazy.spawn('pactl set-sink-volume 0 -2%')),
+    Key([], 'XF86AudioRaiseVolume', lazy.spawn('pactl set-sink-volume 0 +2%')),
 
     # Applications
-    Key([mod], "Return", lazy.spawn(term)),
+    Key([mod], 'Return', lazy.spawn(term)),
+    Key([mod], 'e', lazy.spawn('nemo')),
+    Key([mod], 'c', lazyBrowser('chrome://newtab')),
+    Key([mod], 'y', lazyBrowser('https://youtube.com/')),
+    Key([mod], 's', lazyBrowser('https://moodle.mmc.school.nz', profile=1)),
 ]
 
 groups = [Group(str(i)) for i in range(1, 10)]
@@ -54,11 +67,10 @@ groups = [Group(str(i)) for i in range(1, 10)]
 for i in groups:
     keys.extend([
         Key([mod], i.name, lazy.group[i.name].toscreen()),
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
+        Key([mod, 'shift'], i.name, lazy.window.togroup(i.name)),
     ])
 
 layouts = [
-    layout.Max(),
     layout.MonadTall(ratio=0.55, margin=24),
 ]
 
@@ -73,29 +85,29 @@ screens = [
     Screen(
         bottom=bar.Bar(
             [
-                widget.GroupBox(),
-                widget.CurrentLayoutIcon(),
+                widget.GroupBox(this_current_screen_border=colors.blue),
                 widget.Prompt(),
                 widget.WindowName(),
                 widget.Systray(),
-                widget.Mpd2(),
                 widget.Volume(),
-                widget.CheckUpdates(color_have_updates='#acc267'),
-                widget.Clock(format='%H:%M'),
+                widget.CheckUpdates(colour_have_updates=colors.red,
+                                    colour_no_updates=colors.green,
+                                    ),
+                widget.Clock(format='%H:%M', foreground=colors.blue),
             ],
             24,
-            background='#151515',
+            background=colors.black,
             opacity=0.8,
         ),
     ),
 ]
 
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
+    Drag([mod], 'Button1', lazy.window.set_position_floating(),
          start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
+    Drag([mod], 'Button3', lazy.window.set_size_floating(),
          start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+    Click([mod], 'Button2', lazy.window.bring_to_front())
 ]
 
 dgroups_key_binder = None
@@ -121,7 +133,7 @@ floating_layout = layout.Floating(float_rules=[
     {'wmclass': 'ssh-askpass'},  # ssh-askpass
 ])
 auto_fullscreen = True
-focus_on_window_activation = "smart"
+focus_on_window_activation = 'smart'
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
@@ -131,4 +143,4 @@ focus_on_window_activation = "smart"
 #
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
-wmname = "LG3D"
+wmname = 'LG3D'
