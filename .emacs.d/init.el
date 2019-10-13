daca604a39093492679f3a348a16f985e61b5c8d
Benjamin Davies <bentendavies@gmail.com>
2019-10-14 07:57:46 +1300

    Add relative line numbers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,28 +18,45 @@
  ;; If there is more than one, they won't work right.
  )
 
+;; Install packages
 (package-install 'evil)
 (package-install 'auctex)
+(package-install 'linum-relative)
 
+;; Set cursor and visual-bell
 (set-default 'cursor-type 'hbar)
 (setq visible-bell nil
       ring-bell-function (lambda ()
 			   (invert-face 'mode-line)
 			   (run-with-timer 0.1 nil #'invert-face 'mode-line)))
 
+;;; Asthetics
+;; Use different font size for mac
 (if (eq system-type 'darwin)
     (add-to-list 'default-frame-alist
 		 '(font . "Input Mono-15"))
   (add-to-list 'default-frame-alist
 	       '(font . "Input Mono-11")))
 (load-theme 'wombat)
+(invert-face 'mode-line)
 
-(evil-mode)
-
+;;; Disable GUI
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-(column-number-mode)
+;;; Functionality modes
+(evil-mode)
 (show-paren-mode)
 (electric-pair-mode)
+
+;;; Line numbers
+(linum-mode)
+(linum-relative-global-mode)
+(setq linum-relative-current-symbol "")
+
+;;; Modeline modes
+(column-number-mode)
+(display-time-mode)
+(setq display-time-day-and-date t
+      display-time-24hr-format t)
