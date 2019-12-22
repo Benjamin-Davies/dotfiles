bea4797eaf66328c82886b8ebbe75f61eb6308ce
Benjamin-Davies <bentendavies@gmail.com>
2019-12-22 14:18:05 +1300

    More tweaks to emacs config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -2,4 +2,16 @@
 
 ; Fonts
 (setq doom-font (font-spec :family "Input Mono" :size 16)
-      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 16))
+      doom-big-font (font-spec :family "Input Mono" :size 36)
+      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 16)
+
+      projectile-project-search-path '("~/Projects/"))
+
+; Fullscreen on macOS
+(if (eq system-type 'darwin)
+    (toggle-frame-fullscreen))
+
+; EXWM
+(require 'exwm)
+(require 'exwm-config)
+(exwm-config-default)
