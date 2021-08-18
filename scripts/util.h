0d4165e0c0006463abf0fde954c4df01ea9842e5
Benjamin-Davies <bentendavies@gmail.com>
2021-08-18 13:24:50 +1200

    Add unlink

--- scripts/util.h
+++ scripts/util.h
@@ -13,4 +13,6 @@ std::vector<std::string> read_lines(fs::path);
 
 void append_lines_unique(fs::path, std::vector<std::string> &);
 
+void remove_lines_unique(fs::path, std::vector<std::string> &);
+
 bool confirm(std::string);
