fb474d116db2dcfb10c36812d98a9aed4d9a81bf
Benjamin Davies <bentendavies@gmail.com>
2021-11-04 11:49:54 +1300

    Add basic emacs config

---
+++ devel/emacs.d/init.el
@@ -0,0 +1,36 @@
+;; Borrowed from https://github.com/susamn/dotfiles/blob/master/emacs/init.el
+
+;; Configure package.el to include MELPA.
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+
+
+;; Ensure that use-package is installed.
+;;
+;; If use-package isn't already installed, it's extremely likely that this is a
+;; fresh installation! So we'll want to update the package repository and
+;; install use-package before loading the literate configuration.
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file "~/.emacs.d/configuration.org")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-collection undo-tree use-package rustic magit lsp-ui fzf evil dracula-theme company))
+ '(warning-suppress-log-types '(((package reinitialization)) (comp)))
+ '(warning-suppress-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
